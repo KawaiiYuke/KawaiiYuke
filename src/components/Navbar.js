f9feea87220f666a41878c777086d7c30225cc87
hyu25 <angelyu19970627@gmail.com>
2022-08-28 23:40:54 -0400

    navbar icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import "./css/Navbar.css";
 import menu from "../images/menu.png";
 import logo from "../images/kawaii_logo.png";
+import explore from "../images/explore.png";
+import signout from "../images/signout.png";
+import seach from "../images/seach.png";
+import signin from "../images/signin.png";
 
 const Navigation = () => {
   const [navBar, setBar] = useState(false);
@@ -24,21 +28,33 @@ const Navigation = () => {
         </Link>
         <ul>
           <li>
+            <div className="navIcon">
+              <img src={seach} alt="" />
+            </div>
             <input type="search" placeholder="Search Songs" />
           </li>
 
           <li>
             <Link to="/explore">
+              <div className="navIcon">
+                <img src={explore} alt="" />
+              </div>
               <i className="fas fa-home"></i>Explore
             </Link>
           </li>
           <li>
             <Link to="/signin">
+              <div className="navIcon">
+                <img src={signin} alt="" />
+              </div>
               <i className="fas fa-user"></i>Sign In
             </Link>
           </li>
           <li>
             <Link to="/signout">
+              <div className="navIcon">
+                <img src={signout} alt="" />
+              </div>
               <i className="fas fa-address-card"></i>Sign Out
             </Link>
           </li>
