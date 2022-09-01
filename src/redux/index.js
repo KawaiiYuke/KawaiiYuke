88191e9eaea2675431c0d35f9d3974d81456077d
Wei liu <weihuiliu1228sh@gmail.com>
2022-09-01 17:23:11 -0400

    adding redux-persist

--- src/redux/index.js
+++ src/redux/index.js
@@ -4,15 +4,25 @@ import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import logInReducer from "./logIn";
 import BrowseReducer from "./browse";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const reducer = combineReducers({
   logIn: logInReducer,
   browse: BrowseReducer,
 });
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
-const store = createStore(reducer, middleware);
+const store = createStore(persistedReducer, middleware);
+export let persistor = persistStore(store);
 
 export default store;
