c50f02217dac159ca812ca35bc846fb5a8f23c02
Yuri Yoo <yuriyoo@hotmail.com>
2022-09-06 12:00:55 -0400

    fixing search result lyrics

--- src/components/Home.js
+++ src/components/Home.js
@@ -106,9 +106,9 @@ const Home = () => {
                 style={{
                   whiteSpace: "pre",
                   fontWeight: "bold",
-                  color: "#ffffff",
+                  color: "#F0F8FF",
                   backgroundColor: "hsla(0, 100%, 90%, 0.3)",
-                  fontFamily: "Monaco",
+                  justifyContent: "center",
                 }}
               >
                 {lyrics}
