4aea06b8c7d016e0e6a201840f7e93a0c9bf63b9
Wei liu <weihuiliu1228sh@gmail.com>
2022-09-07 10:34:24 -0400

    fix player errors in room component

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Navigation } from "swiper";
@@ -7,28 +7,8 @@ import "swiper/css/effect-fade";
 import "./css/Carousel.scss";
 import Home from "./Home";
 import RoomPlaylist from "./RoomPlaylist";
-import { db } from "./VideoTest";
-import { useSelector } from "react-redux";
-import { doc, getDoc } from "firebase/firestore";
 
 const Carousel = () => {
-  const logInState = useSelector((state) => state.logIn);
-  let accessToken = logInState?.accessToken;
-  const reduxRoomId = useSelector((state) => state.room.roomId);
-  const [playlist, setPlaylist] = useState([]);
-
-  useEffect(() => {
-    async function callPlaylist(reduxRoomId) {
-      const docRef = doc(db, "RoomPlaylist", reduxRoomId);
-      const getdocSnap = await getDoc(docRef);
-      if (getdocSnap.exists()) {
-        const res = getdocSnap.data();
-        if (res) setPlaylist(res.playlist);
-      }
-    }
-    callPlaylist(reduxRoomId);
-  }, []);
-
   return (
     <React.Fragment>
       <div className="swiperContainer" style={{ width: "70%" }}>
