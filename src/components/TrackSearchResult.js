331492ce701ab55c94eab0609048db59684afc22
ymei0072 <yukstormei@gmail.com>
2022-09-06 20:43:16 -0400

    TrackSearchResults code cleanup

--- src/components/TrackSearchResult.js
+++ src/components/TrackSearchResult.js
@@ -1,16 +1,9 @@
-import React from "react";
-import "./css/TrackSearch.css";
-import { useSelector, useDispatch } from "react-redux";
-import app, { db } from "./VideoTest";
-import {
-  collection,
-  addDoc,
-  doc,
-  setDoc,
-  updateDoc,
-  arrayUnion,
-} from "firebase/firestore";
-import { addTrack } from "../redux/roomPlaylist";
+import React from 'react';
+import './css/TrackSearch.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { db } from './VideoTest';
+import { doc, updateDoc, arrayUnion } from 'firebase/firestore';
+import { addTrack } from '../redux/roomPlaylist';
 
 export default function TrackSearchResult({ track, chooseTrack }) {
   function handlePlay() {
@@ -23,24 +16,21 @@ export default function TrackSearchResult({ track, chooseTrack }) {
   async function handlePlaylist(track) {
     if (reduxRoomId) {
       dispatch(addTrack(track));
-      const playlistRef = await updateDoc(
-        doc(db, "RoomPlaylist", reduxRoomId),
-        {
-          playlist: arrayUnion(track),
-        }
-      );
+      await updateDoc(doc(db, 'RoomPlaylist', reduxRoomId), {
+        playlist: arrayUnion(track),
+      });
     }
   }
   return (
     <div
       className="d-flex m-1 justify-content-evenly TrackSearch container"
       style={{
-        cursor: "pointer",
-        display: "flex",
-        justifyContent: "flex-end",
-        backgroundColor: "hsla(0, 100%, 100%, 0.3)",
-        border: "solid",
-        borderColor: "rgb(255, 255, 255, 0.5)",
+        cursor: 'pointer',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        backgroundColor: 'hsla(0, 100%, 100%, 0.3)',
+        border: 'solid',
+        borderColor: 'rgb(255, 255, 255, 0.5)',
       }}
       onClick={handlePlay}
     >
@@ -48,21 +38,21 @@ export default function TrackSearchResult({ track, chooseTrack }) {
         <img
           src={track.albumUrl}
           style={{
-            height: "64px",
-            width: "64px",
-            marginTop: "1em",
+            height: '64px',
+            width: '64px',
+            marginTop: '1em',
           }}
           alt="album"
         />
 
         <div
           style={{
-            fontWeight: "bold",
-            color: "rgb(255, 255, 255, 0.8)",
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-            marginBottom: "0.8em",
+            fontWeight: 'bold',
+            color: 'rgb(255, 255, 255, 0.8)',
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column',
+            marginBottom: '0.8em',
           }}
         >
           {track.title} By {track.artist}
